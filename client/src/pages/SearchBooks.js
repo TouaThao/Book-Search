1e7fb2242a21182263233fcaffa522c0857e8a44
TouaThao <touathao09@gmail.com>
2023-02-07 23:15:28 -0600

    install npm and debug

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -13,7 +13,7 @@ import Auth from "../utils/auth";
 import { saveBook, searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 import { useMutation } from "@apollo/react-hooks";
-import { SAVE_BOOK } from "../utils/mutations";
+import { SAVE_BOOK } from "../utils/mutation";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
